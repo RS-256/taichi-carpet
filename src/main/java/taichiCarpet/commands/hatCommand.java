8aa1e5c1f64939b402398acd9ebe69b8cab07fae
topi-banana <support@topi-banana.com>
2024-03-19 14:25:55 +0900

    [update] commandHat
    add to blocking process, cannot hat items
    + Enchantments.BINDING_CURSE
    + Items.TOTEM_OF_UNDYING
    + Items.SHULKER_BOX(s)
    
    ItemUtils,
    + isShulkerBox
    + isEmptyShulkerBox

--- src/main/java/taichiCarpet/commands/hatCommand.java
+++ src/main/java/taichiCarpet/commands/hatCommand.java
@@ -1,15 +1,24 @@
 package taichiCarpet.commands;
 
+import carpet.script.language.Sys;
 import carpet.utils.CommandHelper;
 import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import taichiCarpet.TaichiCarpetSettings;
+import taichiCarpet.utils.ItemUtils;
+import taichiCarpet.utils.sendMassage;
 
 public class hatCommand {
 
@@ -21,8 +30,22 @@ public class hatCommand {
     }
 
     public static int hatPlayer(ServerPlayerEntity player) {
+        if (player.isSpectator()) return 1;
         ItemStack hat = player.getEquippedStack(EquipmentSlot.HEAD);
         ItemStack stack = player.getMainHandStack();
+
+        Item item = stack.getItem();
+        if (EnchantmentHelper.fromNbt(hat.getEnchantments()).containsKey(Enchantments.BINDING_CURSE)) {
+            sendMassage.sendSourceMessage(player, "Already equipped with an Item with BINDING_CURSE");
+            return 1;
+        } else if (item.equals(Items.TOTEM_OF_UNDYING)) {
+            sendMassage.sendSourceMessage(player, "Items that cannot be equipped: TOTEM_OF_UNDYING");
+            return 1;
+        } else if (ItemUtils.isShulkerBox(item) && ItemUtils.isEmptyShulkerBox(stack)) {
+            sendMassage.sendSourceMessage(player, "Items that cannot be equipped: SHULKER_BOX(notEmpty)");
+            return 1;
+        }
+
         ItemStack stackCopy = stack.copy();
         stackCopy.setCount(1);
         player.equipStack(EquipmentSlot.HEAD, stackCopy);
---
+++ src/main/java/taichiCarpet/utils/ItemUtils.java
@@ -0,0 +1,45 @@
+package taichiCarpet.utils;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtList;
+
+public class ItemUtils {
+    public static boolean isShulkerBox(Item item) {
+        return item.equals(Items.SHULKER_BOX) ||
+                item.equals(Items.BLACK_SHULKER_BOX) ||
+                item.equals(Items.BLUE_SHULKER_BOX) ||
+                item.equals(Items.BROWN_SHULKER_BOX) ||
+                item.equals(Items.CYAN_SHULKER_BOX) ||
+                item.equals(Items.GRAY_SHULKER_BOX) ||
+                item.equals(Items.GREEN_SHULKER_BOX) ||
+                item.equals(Items.LIGHT_BLUE_SHULKER_BOX) ||
+                item.equals(Items.LIGHT_GRAY_SHULKER_BOX) ||
+                item.equals(Items.LIME_SHULKER_BOX) ||
+                item.equals(Items.MAGENTA_SHULKER_BOX) ||
+                item.equals(Items.ORANGE_SHULKER_BOX) ||
+                item.equals(Items.PINK_SHULKER_BOX) ||
+                item.equals(Items.PURPLE_SHULKER_BOX) ||
+                item.equals(Items.RED_SHULKER_BOX) ||
+                item.equals(Items.WHITE_SHULKER_BOX) ||
+                item.equals(Items.YELLOW_SHULKER_BOX);
+    }
+
+    public static boolean isEmptyShulkerBox(ItemStack stack) {
+        NbtCompound nbt = stack.getNbt();
+        if (nbt != null) {
+            NbtCompound blockEntityTagNbt = nbt.getCompound("BlockEntityTag");
+            if (blockEntityTagNbt != null) {
+                if (blockEntityTagNbt.contains("Items")) {
+                    NbtList itemList = blockEntityTagNbt.getList("Items", 10);
+                    if (itemList.isEmpty()) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
